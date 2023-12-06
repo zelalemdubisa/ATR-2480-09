b32aee51b62655093ae28c05beca1c75832decc2
Ayalkbet Zelalem <46752726+zelalemdubisa@users.noreply.github.com>
2023-12-06 14:04:33 +0300

    footer done :sparkles:

---
+++ src/script/script.js
@@ -0,0 +1,12 @@
+var hamburger = document.getElementById("hamburger");
+var navbar = document.getElementById("navbar");
+
+hamburger.addEventListener("click", function () {
+  if (navbar.style.display === "none") {
+    navbar.style.display = "grid";
+    hamburger.textContent = "X";
+  } else {
+    navbar.style.display = "none";
+    hamburger.textContent = "☰";
+  }
+});

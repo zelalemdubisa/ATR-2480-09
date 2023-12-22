f0e7a71cf4a98910d00714acc1058852df08762d
Ayalkbet Zelalem <46752726+zelalemdubisa@users.noreply.github.com>
2023-12-22 11:42:11 +0300

    landing page done :sparkles:

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        lightGrey:"#444",
+        darkGrey:"#333"
+      }
+    },
+  },
+  plugins: [],
+}
+
